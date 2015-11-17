5a3728ced45da4727c7dc4fdcd0060f40aecb97c
ZachLiuGIS <zachliugis@gmail.com>
2015-11-17 17:15:30 -0500

    added Queue and Stack in algorithms/data_structures

---
+++ algorithms/data_structures/Queue.py
@@ -0,0 +1,16 @@
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
