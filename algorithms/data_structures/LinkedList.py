7f7fe0b8b6834fec6669652bb7552e3ae5c0c397
alireza <ali@Alirezas-MacBook-Air.local>
2019-03-16 21:16:17 -0700

    cleaned up part the code, still the same functionality but less code that's all

--- algorithms/data_structures/LinkedList.py
+++ algorithms/data_structures/LinkedList.py
@@ -1,17 +1,7 @@
-class Node(object):
-
+ss Node(object):
     def __init__(self, data=None, next=None):
         self.data = data
         self.next = next
-
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
-    def set_next(self, new_next):
-        self.next = new_next
 
 
 class LinkedList(object):
@@ -21,7 +11,7 @@ class LinkedList(object):
 
     def insert(self, data):
         new_node = Node(data)
-        new_node.set_next(self.head)
+        new_node.next=self.head
         self.head = new_node
 
     def size(self):
@@ -29,38 +19,48 @@ class LinkedList(object):
         count = 0
         while current:
             count += 1
-            current = current.get_next()
+            current = current.next
         return count
 
     def search(self, data):
         current = self.head
         while current:
-            if current.get_data() == data:
+            if current.data == data:
+                print("found it")
                 return current
             else:
-                current = current.get_next()
+                current = current.next
         return None
 
     def delete(self, data):
         current = self.head
         prev = None
         while current:
-            if current.get_data() == data:
+            if current.data == data:
                 if current == self.head:
-                    self.head = current.get_next()
+                    self.head = current.next
                 else:
-                    prev.set_next(current.get_next())
+                    prev.next=current.next
                 return current
             prev = current
-            current = current.get_next()
+            current = current.next
         return None
 
     def print(self):
-        lst = []
         current = self.head
-        while current:
-            lst.append(str(current.get_data()))
-            current = current.get_next()
-        print('->'.join(lst))
+        while current != None:
+            print(current.data,end="-")
+            current = current.next
+        print(" ")
+        
 
+# Example of how to use it
+# lk = LinkedList()
+# lk.insert("a")
+# lk.insert("b")
+# lk.insert("c")
+# lk.print()
+# lk.search("d")
+# lk.delete("c")
+# lk.print()
 
